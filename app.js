2d07090dac2d55134db544a7277eb2ea7d52d656
AkashSoren <akash.soren.93@gmail.com>
2022-11-09 12:08:33 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
+
+const date = document.getElementById("date") 
+date.innerHTML = new Date().getFullYear();
+console.log(date)
+
+
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
